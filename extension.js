94dcc15d068cbec7261779bb1ba4944d24cefdbc
PearlNegra <usernamejuntos@live.com>
2015-06-17 21:28:14 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -35,7 +35,7 @@
          */
 
         bot.commands.baconCommand = {
-            command: ['kaz', 'fu'],  //The command to be called. With the standard command literal this would be: !bacon
+            command: ['fu', 'lor', 'tau'],  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
